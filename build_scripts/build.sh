e2eead71b4c0cd2075ce81390817adc3e36b5364
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2026-06-12 12:17:01 +0200

    build more notebooks

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -6,6 +6,8 @@ cp build_scripts/webgui.py `python -c "import netgen.webgui; print(netgen.webgui
 python build_scripts/clean_built_notebooks.py unit1/*.ipynb
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace unit1/*.ipynb
 jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.store_widget_state=True --ExecutePreprocessor.timeout=600 ./unit1/elasticity2D.ipynb
+jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.store_widget_state=True --ExecutePreprocessor.timeout=600 ./unit1/linearized_elasticity.ipynb
+jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.store_widget_state=True --ExecutePreprocessor.timeout=600 ./unit1/elasticity3D.ipynb
 python build_scripts/export_webgui_assets.py --source build_scripts/webgui.py --out-dir unit1
 python build_scripts/widgets_to_directives.py ./unit1/*.ipynb
 jupyter book build --html
